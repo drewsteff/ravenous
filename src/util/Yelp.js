b2e1ddf7eb4f60a908a42b42f82aaa92c74e8d5d
Andrew Steffenhagen <andrewsteffenhagen@Andrews-MacBook-Pro.local>
2020-10-13 12:38:16 -0500

    added handleSortByChange

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,30 +1,30 @@
 const apiKey = 'CgkJ58d3q5DemWFiykxw9q1no8yiBOJlDxhzs1PGAOgH5gWLvMk-8zmvU42nYXjeAle-PiS3Fhy7qV7g0iF_ihyExeXEww2sY81l4Jw15deG-vO1oOA83MlxLZCFX3Yx';
 
 const Yelp = {
-    search(term, location, sortBy) {
-      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      }).then(response => {
-        return response.json();
-      }).then(jsonResponse => {
-        if (jsonResponse.businesses) {
-          return jsonResponse.businesses.map(business => ({
-            id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            address: business.location.address1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories[0].title,
-            rating: business.rating,
-            reviewCount: business.review_count
-          }));
-        }
-      });
-    }
-  };
-  
-  export default Yelp;
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
